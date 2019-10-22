d01038ad53d11fe196ea5d5671918820d51ccf51
Anthony Angelo <angelo_anthony@bah.com>
2019-10-22 19:49:16 -0400

    Update

--- nowcli/now.py
+++ nowcli/now.py
@@ -16,19 +16,20 @@ def main():
 
 # Top Level Parser
 parser = argparse.ArgumentParser(description="ServiceNow (NOW) Command Line Interface (CLI)", prog='now')
+parser.add_argument('configure', type=str, help='configure options')
 parser.add_argument('-p','--profile', type=str, help='profile')
-parser.add_argument('-c','--configure', type=str, help='configure options')
 parser.add_argument('-x','--socks5', action='store_true', help='use SOCKS5 proxy on localhost port 5555')
 parser.add_argument('-v','--version', action='version', version=__version__)
 
 # Commands Parser
-subparsers = parser.add_subparsers(title='commands', description='Valid NOW CLI commands', help='command help')
+commands = parser.add_subparsers(title='commands', description='Valid NOW CLI commands', help='command help')
 
 # IM Command Parser
-im_parser = subparsers.add_parser('im', description='Incident Management Command')
+im_command = commands.add_parser('im', description='Incident Management Command')
+im_command.add_argument('create-incident', help='create an incident')
 
 # EM Command Parser
-em_parser = subparsers.add_parser('em', description='Event Management Command')
+em_command = commands.add_parser('em', description='Event Management Command')
 
 # Parse Arguments
 args = parser.parse_args()
