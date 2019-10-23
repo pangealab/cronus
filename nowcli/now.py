db13123650deeec7a51299615e329a6d72b7a8b6
Anthony Angelo <angelo_anthony@bah.com>
2019-10-23 00:37:09 -0400

    Update

--- nowcli/now.py
+++ nowcli/now.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 __version__ = "1.0.0"
@@ -16,20 +16,20 @@ def main():
 
 # Top Level Parser
 parser = argparse.ArgumentParser(description="ServiceNow (NOW) Command Line Interface (CLI)", prog='now')
-parser.add_argument('configure', type=str, help='configure options')
-parser.add_argument('-p','--profile', type=str, help='profile')
-parser.add_argument('-x','--socks5', action='store_true', help='use SOCKS5 proxy on localhost port 5555')
-parser.add_argument('-v','--version', action='version', version=__version__)
+# parser.add_argument('configure', type=str, help='configure options')
+# parser.add_argument('-p','--profile', type=str, help='profile')
+# parser.add_argument('-x','--socks5', action='store_true', help='use SOCKS5 proxy on localhost port 5555')
+# parser.add_argument('-v','--version', action='version', version=__version__)
 
-# Commands Parser
-commands = parser.add_subparsers(title='commands', description='Valid NOW CLI commands', help='command help')
+commands_parser = parser.add_subparsers(help='commands')
 
-# IM Command Parser
-im_command = commands.add_parser('im', description='Incident Management Command')
-im_command.add_argument('create-incident', help='create an incident')
+# IM Parser
+im_parser = commands_parser.add_parser('im', help='incident management')
+im_parser.add_argument('create-incident', help='create an incident')
 
-# EM Command Parser
-em_command = commands.add_parser('em', description='Event Management Command')
+# EM Parser
+em_parser = commands_parser.add_parser('em', help='event management')
+em_parser.add_argument('create-event', help='create an event')
 
 # Parse Arguments
 args = parser.parse_args()
