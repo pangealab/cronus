d585882e9ed2e5276c992de39f3d721988f49ca5
Anthony Angelo <anthony.angelo@servicenow.com>
2020-01-08 22:32:29 -0500

    Update

---
+++ cronus/profile.py
@@ -0,0 +1,29 @@
+# Local Imports
+import cronus.properties as properties
+
+# Repo Imports
+import configparser
+import logging
+
+# Set Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def get_props(profile):
+
+    config = configparser.ConfigParser()
+    config.read(properties.NOW_CONFIG)
+
+    # Return the DEFAULT section if no PROFILE provided
+    if not profile:
+        defaults = config.defaults()
+        return defaults
+    else:
+        # Return the PROFILE section if it exists
+        if config.has_section(profile):
+            items = config[profile]
+            return items
+        # Create the PROFILE section if it does not exists
+        else:
+            defaults = config.defaults()
+            return defaults
