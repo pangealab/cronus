5ec565c9a3dd063d658fecf022f68add2b6bbca4
Anthony Angelo <anthony.angelo@servicenow.com>
2020-06-18 13:55:15 -0400

    Update

---
+++ cronus/devops.py
@@ -0,0 +1,14 @@
+# Local Imports
+from cronus import properties
+
+# Repo Imports
+import profile
+import logging
+import configparser
+
+# Set Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    print("Called DevOps ...")
