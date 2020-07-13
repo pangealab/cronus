f16ad98b700659c7d6772f0aa5f4710d8eed57ab
ganesh.bhat <ganesh.bhat@servicenow.com>
2020-07-13 21:53:47 +0530

    Revert "added polling logic and progress indicator"
    This reverts commit 00ce3af340a2af69d1c6ee9bb326d731d9e83196.

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ import re
 import os
 from setuptools import find_packages, setup
 
-DEPENDENCIES = ["requests","halo", "polling2"]
+DEPENDENCIES = ["requests"]
 EXCLUDE_FROM_PACKAGES = ["contrib", "docs", "tests*"]
 CURDIR = os.path.abspath(os.path.dirname(__file__))
 
 with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
     README = f.read()
+
 
 def get_version():
     main_file = os.path.join(CURDIR, "cronus", "now.py")
@@ -29,7 +30,6 @@ def get_version():
         match = _version_re.search(f.read())
         version = match.group("version") if match is not None else '"unknown"'
     return str(ast.literal_eval(version))
-
 
 setup(
     name="nowcli",
